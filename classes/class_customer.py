8dc38009e3ebb672b7e9ba5ad75f9c9ead1a86ca
CristianaRego <up202407467@up.pt>
2025-04-04 16:10:27 +0100

    segundo commit

--- classes/class_customer.py
+++ classes/class_customer.py
@@ -1,13 +1,6 @@
-#Class Customer manteve-se Class Customer
-
-
-#removi os setters, acho que após uma subscription em princípio não se 
-#alterarão os dados de uma determinada fidelização
-
-
-#também acrescentei o datetime para formatarmos dados temporais, mas podemos
-#retirar se virem que não é útil ordenar dados por ordem de aparecimento.
+#Class Customer
 import datetime 
+from classes.class_devices import Devices
 from classes.gclass import Gclass
 
 class Customer(Gclass):
@@ -16,25 +9,30 @@ class Customer(Gclass):
     pos = 0
     sortkey = ''
     # class attributes, identifier attribute 'id' must be the first on the list
-    att = ['_id','_email','_signup_date']
+    att = ['_id', 'devices_id', 'name', '_email', '_signup_date']
     # Class header title
     header = 'Customer'
     # field description for use in, for example, input form
-    des = ['Id','Email','Signup_Date']
+    des = ['Id', 'Devices_Id', 'Name', 'Email', 'Signup_Date']
     # Constructor: Called when an object is instantiated
-    #Cristiana: retirei o atributo phone e adicionei o name
-    def __init__(self, id, name, email, signup_date):
+
+
+    def __init__(self, id, devices_id, name, email, signup_date):
         super().__init__()
         # Object attributes
         id = Customer.get_id(id)
-        self._id = id
-        self._name = name
-        self._email = email
-        temp = list(map(int,signup_date.split('/')))
-        self._signup_date = datetime.date(temp[0],temp[1],temp[2])
-        # Add the new object to the Customer's list
-        Customer.obj[id] = self
-        Customer.lst.append(id)
+        if int(devices_id) in Devices.lst:
+            self._id = id
+            self._devices_id= int(devices_id)
+            self._name = name
+            self._email = email
+            temp = list(map(int,signup_date.split('/')))
+            self._signup_date = datetime.date(temp[0],temp[1],temp[2])
+            # Add the new object to the Customer's list
+            Customer.obj[id] = self
+            Customer.lst.append(id)
+        else:
+            print('Device ', devices_id, ' not found') 
     
     # Object properties
     @property
@@ -42,8 +40,13 @@ class Customer(Gclass):
         return self._id
     
     @property
+    def devices_id(self):
+        return self._devices_id
+    
+    @property
     def name(self):
         return self._name
+    
 
     @property
     def email(self):
@@ -53,13 +56,16 @@ class Customer(Gclass):
     def signup_date(self):
         return self._signup_date
     
-#Cristiana: acrescentei o setter do email pq o cliente pode mudar de endereço de email após a fidelização
-
     @id.setter 
     def id(self,new_id):
         self._id = new_id
         return self._id
 
+    @devices_id.setter 
+    def devices_id(self,new_id):
+        self._devices_id = new_id
+        return self._devices_id
+    
     @name.setter 
     def name(self,new_name):
         self._name = new_name
--- classes/class_subscriptions.py
+++ classes/class_subscriptions.py
@@ -1,6 +1,7 @@
 # Class Subscriptions
+import datetime as dt
 from classes.class_customer import Customer
-from classes.class_devices import Devices
+from classes.class_plans import Plans
 # Import the generic class
 from classes.gclass import Gclass
 
@@ -10,29 +11,32 @@ class Subscriptions(Gclass):
     pos = 0
     sortkey = ''
     # class attributes, identifier id, attribute must be the first on the list
-    att = ['_id', '_customer_id','_devices_id']
+    att = ['_id', '_plans_id', '_customer_id', '_start_date', '_status']
     # Class header title
     header = 'Subscriptions'
     # field description for use in, for example, input form
-    des = ['Id','Customer_id','Devices_id']
+    des = ['Id','Plans_Id','Customer_Id', 'Start_Date', 'Status']
     # Constructor: Called when an object is instantiated
-    def __init__(self, id, customer_id, devices_id):
+    def __init__(self, id, plans_id, customer_id, start_date, status):
         super().__init__()
         # Object attributes
         # Check the customer and devices referential integrity
         customer_id = int(customer_id)
-        devices_id = int(devices_id)
-        if customer_id in Customer.lst:
-            if devices_id in Devices.lst:
-                id = Subscriptions.get_id(id)
+        plans_id = int(plans_id)
+        if int(customer_id) in Customer.lst:
+            if int(plans_id) in Plans.lst:
+                id = Customer.get_id(id)
                 self._id = id
-                self._customer_id = customer_id
-                self._devices_id = devices_id
+                self._customer_id = int(customer_id)
+                self._plans_id = int(plans_id)
+                temp = list(map(int,start_date.split('/')))
+                self._start_date = dt.date(temp[0],temp[1],temp[2])
+                self._status = status
                 # Add the new object to the Subscription list
                 Subscriptions.obj[id] = self
                 Subscriptions.lst.append(id)
             else:
-                print('Device ', devices_id, ' not found')
+                print('Plans ', plans_id, ' not found')
         else:
             print('Customer ', customer_id, ' not found')
     # Object properties
@@ -40,14 +44,28 @@ class Subscriptions(Gclass):
     @property
     def id(self):
         return self._id
-    # order property getter method
+    
+    # customer_id property getter method
     @property
     def customer_id(self):
         return self._customer_id
-    # product property getter method
+    
+    # plans_id property getter method
     @property
-    def devices_id(self):
-        return self._devices_id
+    def plans_id(self):
+        return self._plans_id
+    
+    # start_date property getter method
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    # status property getter method
+    @property
+    def status(self):
+        return self._status
+    
+    
     
     @id.setter 
     def id(self,new_id):
@@ -59,8 +77,18 @@ class Subscriptions(Gclass):
         self._customer_id = new_customer_id
         return self._customer_id
     
-    @devices_id.setter 
-    def id(self,new_devices_id):
-        self._devices_id = new_devices_id
-        return self._devices_id
+    @plans_id.setter 
+    def plans_id(self,new_plans_id):
+        self._plans_id = new_plans_id
+        return self._plans_id
+    
+    @start_date.setter 
+    def start_date(self,new_start_date):
+        self._start_date = new_start_date
+        return self._start_date
+    
+    @status.setter 
+    def status(self,new_status):
+        self._status = new_status
+        return self._status
   
